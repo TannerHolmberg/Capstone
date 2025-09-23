5420b4f7be18f4587c0cc573fd26735f965fe7f5
Tanner Holmberg <tannerholmberg5@gmail.com>
2025-09-23 09:58:28 -0500

    Made changes to login look

--- src/App.js
+++ src/App.js
@@ -23,12 +23,23 @@ function App() {
   };
 
   return (
-    <div>
-      <input placeholder="Email" onChange={e => setEmail(e.target.value)} />
-      <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
-      <button onClick={signup}>Sign Up</button>
-      <button onClick={login}>Login</button>
-      <button onClick={logout}>Logout</button>
+    <div className = "Background">
+      <div id="LoginForm">
+        <h3 id="LoginHeader">Login</h3>
+        <div className="inputField" id="emailField">
+          <label>Email:</label>
+          <input className="" placeholder="Email" onChange={e => setEmail(e.target.value)} />
+        </div>
+        <div className="inputField">
+          <label>Password:</label>
+          <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+        </div>
+        <div id="buttonContainer">
+          <button id="signUp" onClick={signup}>Sign Up</button>
+          <button onClick={login}>Login</button>
+          <button onClick={logout}>Logout</button>
+        </div>
+      </div>
     </div>
   );
 }
