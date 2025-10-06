5e83b0f60df00b4c4a52e8a011f762e062ac3c12
Tanner Holmberg <tannerholmberg5@gmail.com>
2025-10-06 13:39:07 -0500

    Added bullitin board section and whiteboard section

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -5,6 +5,9 @@ import logo from "./images/ClassroomConnect.png";
 import books from "./images/books.png";
 import goal from "./images/goal.png";
 import ChalkTray from "./Components/ChalkTray";
+import dollarBag from "./images/dollarBag.png";
+import family from "./images/family.png";
+import wallet from "./images/Wallet.png";
 
 function LandingPage() {
   return (
@@ -39,12 +42,37 @@ function LandingPage() {
                     <button className="PostListingsButton">Post<br />Listings</button>
                 </div>
             </div>
-
         </div>
         <ChalkTray />
+        <div className="bullitenDivider"></div>
+        <section className="bullitenBoard">
+          <div className="bullitenBorderLR"></div>
+          <div className="bullitenBorderLR"></div>
+        </section>
+        <div className="bullitenDivider"></div>
 
-      <h1>Welcome to Classroom Connect</h1>
-      <p>Connecting parents and teachers to support classroom needs.</p>
+        <section className="WhiteBoard">
+          <div className="HowItWorks">
+            <h2>Why It Matters</h2>
+          </div>
+          <div className="stepsIconsContainer">
+            <img src={dollarBag} alt="Dollar Bag" />
+            <img src={wallet} alt="Wallet" />
+            <img src={family} alt="Family" />
+          </div>
+          <div className="StepsContainer">
+            <div className="Step">
+              <p><span style={{ color: 'red' }}>94%</span> of teachers spend their own money on supplies.</p>
+            </div>
+            <div className="Step" id="middle-step">
+              <p>The Average teacher spends <span style={{ color: 'blue' }}>$750</span> per year out of pocket.</p>
+            </div>
+            <div className="Step" id="last-step">
+              <p><span style={{ color: 'rgb(255, 0, 255)' }}>Many</span> Parents want to help but don’t always know how.</p>
+            </div>
+          </div>
+          
+        </section>
     </div>
   );
 }
