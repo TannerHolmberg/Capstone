c1c80ee9ac0cc99664f339adc8a0bc89a3de17a0
noahm02Stu <noahmarsh@my.unt.edu>
2025-10-13 18:36:20 -0500

    Updates to login page
    Made the login page look nicer. Still need to add the adaptive appearance and navlink to the sign in button

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -3,6 +3,7 @@ import logo from "./images/ClassroomConnect.png";
 import ChalkTray from "./Components/ChalkTray";
 import Navbar from "./Components/LoginSignUpLandingNavbar";
 import "./LoginPage.css";
+import { NavLink } from "react-router-dom";
 
 function LoginPage() {
   return (
@@ -10,19 +11,19 @@ function LoginPage() {
       <Navbar />
       <div className="login-container">
         <div className="login-logo-quote-container">
-          <img className="login-Logo" src={logo} alt="Classroom Connect Logo" />
+          <img className="login-logo" src={logo} alt="Classroom Connect Logo" />
           <div className="login-quote">
             <h1>Supporting Teachers,<br /> &nbsp;&nbsp;Empowering <br /> &nbsp;Classrooms.</h1>
           </div>
         </div>
-      </div>
-      <div className="login-form">
-        <form>
-          <h2>Login</h2>
-            <input type="email" placeholder="Email" required />
-            <input type="password" placeholder="Password" required />
-            <button type="submit">Sign In</button>
-        </form>
+        <div className="login-form">
+          <form>
+            <h2>Login</h2>
+              <input type="email" placeholder="Email" required />
+              <input type="password" placeholder="Password" required />
+              <button type="submit">Sign In</button>
+          </form>
+        </div>
       </div>
       <ChalkTray />
     </div>
